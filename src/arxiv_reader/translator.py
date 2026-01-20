7c0f2497f0aaf65e720c4bb88052b479e88d0c71
Axi404 <axihelloworld@gmail.com>
2026-01-20 18:53:43 +0800

    [feat] update favourite

--- src/arxiv_reader/translator.py
+++ src/arxiv_reader/translator.py
@@ -169,7 +169,7 @@ class GPTTranslator:
                         {"role": "user", "content": prompt},
                     ],
                     temperature=0.2,
-                    max_tokens=300,
+                    max_tokens=2000,
                     response_format={"type": "json_object"},
                 )
 
